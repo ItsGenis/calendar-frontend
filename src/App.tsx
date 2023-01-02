a13d03a0fecd26271b55c676968b475f6593a0ee
Genís <genis.matutes@gmail.com>
2023-01-02 18:40:26 +0100

    feat: set up Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+interface Event {
+  id: string,
+  title: string,
+  description: string,
+  startsAt: Date,
+  endsAt: Date
+}
+
+const GET_EVENTS = gql`
+  query GetEvents {
+    events {
+      id
+      title
+      description
+      startsAt
+      endsAt
+    }
+  }
+`;
+
+function DisplayEvents() {
+  const { loading, error, data } = useQuery(GET_EVENTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.events.map(({ id, title, description, startsAt, endsAt }: Event) => (
+    <div key={id}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <p>{startsAt.toString()}</p>
+      <p>{endsAt.toString()}</p>
+      <br />
+    </div>
+  ));
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Events</h2>
+      <DisplayEvents />
     </div>
   );
 }
