4e57e78cdb48e51e969c4e44ac7d80cd28522bbf
Genís <genis.matutes@gmail.com>
2023-01-05 15:58:21 +0100

    feat: add layout styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Layout } from 'antd';
 import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
 
 function App() {
-  return <HomePage />;
+  return (
+    <Layout>
+      <Layout.Header style={{ color: 'white' }}>
+        <h1>Calendar APP</h1>
+      </Layout.Header>
+      <Layout.Content style={{ padding: '50px' }}>
+        <HomePage />
+      </Layout.Content>
+    </Layout>
+  );
 }
 
 export default App;
